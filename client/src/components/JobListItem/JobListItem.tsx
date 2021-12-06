b7211abd3c9b6b5a92a7ac3757da4088ba4a0517
NSJTeo <n.teomacdonald@gmail.com>
2021-12-06 08:16:15 -0800

    Fix public folder structure

--- client/src/components/JobListItem/JobListItem.tsx
+++ client/src/components/JobListItem/JobListItem.tsx
@@ -7,5 +7,9 @@ interface Props {
 
 export const JobListItem = ({ jobPosting }: Props): ReactElement => {
   console.log(jobPosting);
-  return <div></div>;
+  return (
+    <div>
+      <img src={`http://localhost:8080` + jobPosting.logo.slice(1)} alt="" />
+    </div>
+  );
 };
