1c0782983e69daf18b24e7cfcc6c726517909335
NSJTeo <n.teomacdonald@gmail.com>
2021-12-06 10:04:14 -0800

    Display job details on details component

--- client/src/pages/Details.tsx
+++ client/src/pages/Details.tsx
@@ -1,13 +1,46 @@
 import React, { ReactElement, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { JobPosting } from "../types/types";
 import axios from "axios";
 
 export const Details = (): ReactElement => {
+  const [jobDetails, setJobDetails] = useState<JobPosting>();
+
   const params = useParams();
+
   useEffect(() => {
     axios.get(`http://localhost:8080/jobs/` + params.jobID).then((response) => {
-      console.log(response.data);
+      setJobDetails(response.data);
     });
   }, []);
-  return <div>Job Details</div>;
+
+  if (!jobDetails) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <p>{jobDetails.postedAt}</p>
+      <p>{jobDetails.contract}</p>
+      <h1>{jobDetails.position}</h1>
+      <p>{jobDetails.location}</p>
+      <button>Apply Now</button>
+      <p>{jobDetails.description}</p>
+      <h2>Requirements</h2>
+      <p>{jobDetails.requirements.content}</p>
+      <ul>
+        {jobDetails.requirements.items.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
+      <h2>What You Will Do</h2>
+      <p>{jobDetails.role.content}</p>
+      <ul>
+        {jobDetails.role.items.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
+      <button>Apply Now</button>
+    </>
+  );
 };
