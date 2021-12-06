cb87aed25bd3af93be5d77b6ad1778b2d5151b9b
NSJTeo <n.teomacdonald@gmail.com>
2021-12-05 23:03:44 -0800

    Create Express application

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,1 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8000;
+app.use((0, cors_1.default)());
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
 require("dotenv").config();
+import express from "express";
+import cors from "cors";
+const app = express();
+
+const PORT: string | number = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
