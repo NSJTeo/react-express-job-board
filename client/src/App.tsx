3ff894ea1661a583e0c32f90137bb1dd26e1f3a3
NSJTeo <n.teomacdonald@gmail.com>
2021-12-07 10:08:53 -0800

    Home page container styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Details } from "./pages/Details";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home/Home";
 import { PageHeader } from "./components/PageHeader/PageHeader";
 import "./App.scss";
 
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import axios from "axios";
-import { JobPosting } from "../types/types";
-import { JobListItem } from "../components/JobListItem/JobListItem";
+import { JobPosting } from "../../types/types";
+import { JobListItem } from "../../components/JobListItem/JobListItem";
+import "./Home.scss";
 
 interface Props {}
 
@@ -13,9 +14,11 @@ export const Home = (props: Props): ReactElement => {
       setJobPostings(response.data);
     });
   }, []);
+
   return (
     <>
-      <ul>
+      <div className="home__filter-container"></div>
+      <ul className="home__job-list">
         {jobPostings.map((jobPosting) => {
           return <JobListItem key={jobPosting.id} jobPosting={jobPosting} />;
         })}
