71afc67ceb5f8553c773c9f3a8bd5427941bb4f9
NSJTeo <n.teomacdonald@gmail.com>
2021-12-06 23:45:54 -0800

    PageHeader styles

--- client/src/components/PageHeader/PageHeader.tsx
+++ client/src/components/PageHeader/PageHeader.tsx
@@ -7,6 +7,13 @@ export const PageHeader = (props: Props) => {
   return (
     <header className="page-header">
       <img src="http://localhost:8080/assets/logos/logo.svg" alt="" />
+      <div className="page-header__mode-select-container">
+        <img src="http://localhost:8080/assets/icons/icon-sun.svg" alt="" />
+        <button className="page-header__mode-select">
+          <div className="page-header__mode-select-button"></div>
+        </button>
+        <img src="http://localhost:8080/assets/icons/icon-moon.svg" alt="" />
+      </div>
     </header>
   );
 };
