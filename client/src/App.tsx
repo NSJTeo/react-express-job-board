0ddb4447432c8571495bef2310e6590f995e5928
NSJTeo <n.teomacdonald@gmail.com>
2021-12-06 21:16:44 -0800

    Install sass, variable global partials, job list item styles, app styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,18 @@ import { ReactElement } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Details } from "./pages/Details";
 import { Home } from "./pages/Home";
+import "./App.scss";
 
 function App(): ReactElement {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/job/:jobID" element={<Details />} />
-    </Routes>
+    <div className="app__container">
+      {/* <main className="app__main-section"> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/job/:jobID" element={<Details />} />
+      </Routes>
+      {/* </main> */}
+    </div>
   );
 }
 
--- client/src/components/JobListItem/JobListItem.tsx
+++ client/src/components/JobListItem/JobListItem.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { Link } from "react-router-dom";
 import { JobPosting } from "../../types/types";
+import "./JobListItem.scss";
 
 interface Props {
   jobPosting: JobPosting;
@@ -9,12 +10,23 @@ interface Props {
 export const JobListItem = ({ jobPosting }: Props): ReactElement => {
   console.log(jobPosting);
   return (
-    <li>
+    <li className="job-list-item__container">
       <Link to={`/job/` + jobPosting.id}>
-        <img src={`http://localhost:8080` + jobPosting.logo.slice(1)} alt="" />
-        <p>{jobPosting.postedAt}</p>
-        <p>{jobPosting.contract}</p>
-        <p>{jobPosting.position}</p>
+        <div
+          className="job-list-item__logo-container"
+          style={{ backgroundColor: jobPosting.logoBackground }}
+        >
+          <img
+            src={`http://localhost:8080` + jobPosting.logo.slice(1)}
+            alt=""
+            className="job-list-item__logo"
+          />
+        </div>
+        <div className="job-list-item__time-contract-container">
+          <p>{jobPosting.postedAt}</p>
+          <p>{jobPosting.contract}</p>
+        </div>
+        <h2>{jobPosting.position}</h2>
         <p>{jobPosting.company}</p>
         <p>{jobPosting.location}</p>
       </Link>
