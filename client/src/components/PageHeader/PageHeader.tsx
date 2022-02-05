4f26a17c2e76f5cf346bdf6e7ec2f70dc24ba6ea
nsjteo <n.teomacdonald@gmail.com>
2022-02-05 12:44:19 -0800

    Refactor PageHeader styles into styled-components

--- client/src/components/PageHeader/PageHeader.tsx
+++ client/src/components/PageHeader/PageHeader.tsx
@@ -1,19 +1,52 @@
-import React from "react";
-import "./PageHeader.scss";
+import styled from "styled-components";
 
 interface Props {}
 
+const Header = styled.header`
+  background-image: url("http://localhost:8080/assets/images/bg-pattern-header.svg");
+  height: 8.5rem;
+  padding: 0 1.5rem;
+  padding-top: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const ModeSelectContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ModeSelect = styled.button`
+  width: 3rem;
+  height: 1.5rem;
+  background-color: white;
+  border-radius: 12px;
+  margin: 0 1rem;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border: none;
+`;
+
+const ModeSelectButton = styled.div`
+  width: 0.875rem;
+  height: 0.875rem;
+  border-radius: 50%;
+  background-color: #5964e0;
+`;
+
 export const PageHeader = (props: Props) => {
   return (
-    <header className="page-header">
+    <Header>
       <img src="http://localhost:8080/assets/logos/logo.svg" alt="" />
-      <div className="page-header__mode-select-container">
+      <ModeSelectContainer>
         <img src="http://localhost:8080/assets/icons/icon-sun.svg" alt="" />
-        <button className="page-header__mode-select">
-          <div className="page-header__mode-select-button"></div>
-        </button>
+        <ModeSelect className="page-header__mode-select">
+          <ModeSelectButton />
+        </ModeSelect>
         <img src="http://localhost:8080/assets/icons/icon-moon.svg" alt="" />
-      </div>
-    </header>
+      </ModeSelectContainer>
+    </Header>
   );
 };
