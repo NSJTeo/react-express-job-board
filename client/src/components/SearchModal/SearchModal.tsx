7b99d4d18c1e51a06ff2b37eb2d5666c5469f68f
nsjteo <n.teomacdonald@gmail.com>
2022-02-05 13:05:23 -0800

    Refactor SearchModal styles into styled-components

--- client/src/components/SearchModal/SearchModal.tsx
+++ client/src/components/SearchModal/SearchModal.tsx
@@ -1,9 +1,69 @@
-import React from "react";
-import "./SearchModal.scss";
+import styled from "styled-components";
 
 interface Props {
   setSearchModal: Function;
 }
+
+const Background = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba($color: #000000, $alpha: 0.2);
+  top: 0;
+  left: 0;
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  margin: 1.5rem;
+  background-color: white;
+  width: 100%;
+  border-radius: 6px;
+`;
+
+const LocationFilterContainer = styled.div`
+  border-bottom: 1px solid black;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5rem;
+  height: 4.5rem;
+`;
+
+const LocationFilterIcon = styled.img`
+  margin-right: 1rem;
+`;
+
+const LocationFilterInput = styled.input`
+  border: none;
+  flex-grow: 1;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const ButtonFullTimeCheckboxContainer = styled.div`
+  padding: 1.5rem;
+`;
+
+const FullTimeCheckbox = styled.label`
+  display: flex;
+  align-items: flex-start;
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  background-color: #5964e0;
+  color: white;
+  font-weight: bold;
+  width: 100%;
+  height: 3rem;
+  border: none;
+  border-radius: 5px;
+  margin-top: 1.5rem;
+`;
 
 export const SearchModal = ({ setSearchModal }: Props) => {
   const handleClick = () => {
@@ -11,30 +71,23 @@ export const SearchModal = ({ setSearchModal }: Props) => {
   };
 
   return (
-    <div className="search-modal__background" onClick={handleClick}>
-      <form
-        className="search-modal__form"
-        onClick={(event) => event.stopPropagation()}
-      >
-        <div className="search-modal__location-filter-container">
-          <img
+    <Background onClick={handleClick}>
+      <Form onClick={(event) => event.stopPropagation()}>
+        <LocationFilterContainer>
+          <LocationFilterIcon
             src="http://localhost:8080/assets/icons/icon-location.svg"
             alt=""
-            className="search-modal__location-filter-icon"
           />
-          <input
-            placeholder="Filter by location..."
-            className="search-modal__location-filter-input"
-          />
-        </div>
-        <div className="search-modal__button-full-time-container">
-          <label className="search-modal__contract-checkbox">
+          <LocationFilterInput placeholder="Filter by location..." />
+        </LocationFilterContainer>
+        <ButtonFullTimeCheckboxContainer>
+          <FullTimeCheckbox>
             <input type="checkbox" name="contract" />
             Full Time Only
-          </label>
-          <button className="search-modal__button">Search</button>
-        </div>
-      </form>
-    </div>
+          </FullTimeCheckbox>
+          <Button className="search-modal__button">Search</Button>
+        </ButtonFullTimeCheckboxContainer>
+      </Form>
+    </Background>
   );
 };
