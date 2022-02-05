5fa079f425a9a89259e46d20b586a672371d6d88
nsjteo <n.teomacdonald@gmail.com>
2022-02-05 03:14:32 -0800

    Refactor CompanyInfo styles into styled-components

--- client/src/components/CompanyInfo/CompanyInfo.tsx
+++ client/src/components/CompanyInfo/CompanyInfo.tsx
@@ -1,35 +1,86 @@
 import React from "react";
 import { JobPosting } from "../../types/types";
 import "./CompanyInfo.scss";
+import styled from "styled-components";
 
 interface Props {
   jobDetails: JobPosting;
+  backgroundColor?: String;
 }
 
+interface LogoContainerProps {
+  backgroundColor: string;
+}
+
+const Container = styled.div`
+  background-color: white;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  padding-top: 3.0625rem;
+  top: -1.5625rem;
+  margin-bottom: -1px;
+`;
+
+const LogoContainer = styled.div<LogoContainerProps>`
+  width: 3.125rem;
+  height: 3.125rem;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: -1.5625rem;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+const Name = styled.p`
+  margin-bottom: 0.8125rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+const Website = styled.p`
+  color: #6e8098;
+  margin-bottom: 1.875rem;
+`;
+
+const LinkContainer = styled.div`
+  width: 100%;
+  padding: 0 5.625rem;
+  text-align: center;
+`;
+
+const Link = styled.a`
+  width: 100%;
+  background-color: #5964e0;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+  border-radius: 5px;
+`;
+
 export const CompanyInfo = ({ jobDetails }: Props) => {
+  const backgroundColor = jobDetails.logoBackground;
   return (
     <>
-      <div className="company-info__container">
-        <div
-          className="company-info__logo-container"
-          style={{ backgroundColor: jobDetails.logoBackground }}
-        >
+      <Container>
+        <LogoContainer backgroundColor={backgroundColor}>
           <img
             src={`http://localhost:8080` + jobDetails.logo.slice(1)}
             alt=""
-            className="company-info__logo"
           />
-        </div>
-        <p className="company-info__name">{jobDetails.company}</p>
-        <p className="company-info__website">
-          {jobDetails.website.slice(20) + ".com"}
-        </p>
-        <div className="company-info__link-container">
-          <a href={jobDetails.website} className="company-info__link">
-            Company Site
-          </a>
-        </div>
-      </div>
+        </LogoContainer>
+        <Name>{jobDetails.company}</Name>
+        <Website>{jobDetails.website.slice(20) + ".com"}</Website>
+        <LinkContainer className="company-info__link-container">
+          <Link href={jobDetails.website}>Company Site</Link>
+        </LinkContainer>
+      </Container>
     </>
   );
 };
